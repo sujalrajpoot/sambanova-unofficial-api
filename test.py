dc279803fe7f93f4a435bf46e680b688f01a5810
Sujal Rajpoot <104921805+sujalrajpoot@users.noreply.github.com>
2024-12-19 18:37:06 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from sambanova_unofficial_api import Sambanova_Unofficial_API
-
-if __name__ == "__main__":
-    api = Sambanova_Unofficial_API('_zitok=4XXXXXXXXXXXg')
-    print(f"\nResponse: {api.chat('Hi, who are you?')}")
-    image_path = r'C:\Users\Sujal Rajpoot\Pictures\Saved Pictures\bug in chattebot.png'
-    print(f"\nResponse: {api.vision('Please provide a detailed description of the image, including its contents, colors, and any notable features.', image_path=image_path)}")
